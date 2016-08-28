341b177fe3ddb059e523377fe2508266d961dcfa
blackthorn87 <github@arunmistry.co.uk>
2016-08-28 16:45:35 +0100

    added the page controller extension in _config.php

--- _config.php
+++ _config.php
@@ -3,4 +3,7 @@
 // Extend SiteConfig with the Social Media Links
 Object::add_extension('SiteConfig', 'SocialMediaConfig');
 
+// Extend Page_Controller with the Social Media Links
+Object::add_extension('Page_Controller', 'SocialMediaController');
+
 ?>
